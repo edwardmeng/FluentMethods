c96ea0d8aaf227d4b96c7ccfe0e09dc29ad592ed
Edward Meng <edwardmeng@live.cn>
2016-10-26 17:09:15 +0800

    Add unit test methods for BindValues extension method.

--- src/Core/System.Object/BindValues.cs
+++ src/Core/System.Object/BindValues.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.ComponentModel;
+using System.Linq;
 using System.Reflection;
 
 public static partial class Extensions
@@ -34,29 +35,68 @@ public static partial class Extensions
         public PropertyDescriptor PropertyDescriptor { get; }
     }
 
-    public static void BindValues(this object component, IDictionary fieldValues)
+    /// <summary>
+    /// Binds property values to the specified component.
+    /// </summary>
+    /// <param name="component">The component to bind property values.</param>
+    /// <param name="values">The property values to bind to the specified comonent.</param>
+    public static void BindValues(this object component, IDictionary values)
     {
+        if (values == null) return;
+        if (component == null)
+        {
+            throw new ArgumentNullException(nameof(component));
+        }
         var properties = TypeDescriptor.GetProperties(component);
-        foreach (DictionaryEntry entry in fieldValues)
+        foreach (DictionaryEntry entry in values)
         {
             string propertyName = entry.Key.ToString();
             var property = properties.Find(propertyName, false) ?? properties.Find(propertyName, true);
             if (property != null && !property.IsReadOnly)
             {
                 var propertyValue = entry.Value;
-                var converter = property.Converter;
-                if (entry.Value != null &&
+                if (propertyValue != null &&
 #if NetCore
                     !property.PropertyType.GetTypeInfo().IsInstanceOfType(entry.Value)
 #else
-                    !property.PropertyType.IsInstanceOfType(entry.Value) 
+                    !property.PropertyType.IsInstanceOfType(entry.Value)
 #endif
-                    && converter != null && converter.CanConvertFrom(entry.Value.GetType()))
+                    )
                 {
-                    propertyValue = converter.ConvertFrom(entry.Value);
+                    // Only the type converter marked with TypeConverterAttribute can be affected, the converter marked on the property type should not be used.
+                    var converter = property.Attributes[typeof(TypeConverterAttribute)] != null ? property.Converter : null;
+                    if (converter != null && converter.CanConvertFrom(entry.Value.GetType()))
+                    {
+                        property.SetValue(component, converter.ConvertFrom(entry.Value));
+                        return;
+                    }
                 }
                 property.SetValue(component, propertyValue.ConvertTo(property.PropertyType, new TypeDescriptorContext(property, component)));
             }
         }
+    }
+
+    /// <summary>
+    /// Binds property values to the specified component.
+    /// </summary>
+    /// <param name="component">The component to bind property values.</param>
+    /// <param name="values">The property values to bind to the specified comonent. It can be anonymous object.</param>
+    public static void BindValues(this object component, object values)
+    {
+        if (values == null) return;
+        if (component == null)
+        {
+            throw new ArgumentNullException(nameof(component));
+        }
+        var dictionary = values as IDictionary;
+        if (dictionary != null)
+        {
+            component.BindValues(dictionary);
+            return;
+        }
+        component.BindValues(
+            TypeDescriptor.GetProperties(values)
+                .Cast<PropertyDescriptor>()
+                .ToDictionary(property => property.Name, property => property.GetValue(values)));
     }
 }
