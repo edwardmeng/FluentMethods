757570e4dc932e287199e9d23b1bdb93eb880530
Edward Meng <edwardmeng@live.cn>
2016-11-28 13:00:17 +0800

    Add unit tests.

---
+++ test/build/net45/System.Reflection.Emit.ILGenerator/StoreArg.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace FluentMethods.UnitTests
+{
+    public partial class ILGeneratorFixture
+    {
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArg()
+        {
+            var method = new DynamicMethod("x", typeof(string), new[] { typeof(string) });
+            var il = method.GetILGenerator();
+            il.Emit(OpCodes.Ldstr, "Fizz");
+            il.StoreArg(0);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<string, string>)method.CreateDelegate(typeof(Func<string, string>));
+            Assert.Equal("Fizz", func("Buzz"));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgMuchArguments()
+        {
+            var method = new DynamicMethod("x", typeof(string), new[]
+            {
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),typeof(object),
+                typeof(string)
+            });
+            var il = method.GetILGenerator();
+            il.Emit(OpCodes.Ldstr, "Fizz");
+            il.StoreArg(256);
+            il.Emit(OpCodes.Ldarg,256);
+            il.Emit(OpCodes.Ret);
+
+            var func = (MuchArgumentsHandler<string, string>)method.CreateDelegate(typeof(MuchArgumentsHandler<string, string>));
+            Assert.Equal("Fizz", func(
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                "Buzz"));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgChar()
+        {
+            var method = new DynamicMethod("x", typeof(char), new[] { typeof(char) });
+            var il = method.GetILGenerator();
+            il.StoreArg(0, (char)20);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<char, char>)method.CreateDelegate(typeof(Func<char, char>));
+            Assert.Equal((char)20, func((char)30));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgByte()
+        {
+            var method = new DynamicMethod("x", typeof(byte), new[] { typeof(byte) });
+            var il = method.GetILGenerator();
+            il.StoreArg(0, (byte)20);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<byte, byte>)method.CreateDelegate(typeof(Func<byte, byte>));
+            Assert.Equal(20, func(30));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgSByte()
+        {
+            var method = new DynamicMethod("x", typeof(sbyte), new[] { typeof(sbyte) });
+            var il = method.GetILGenerator();
+            il.StoreArg(0, (sbyte)20);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<sbyte, sbyte>)method.CreateDelegate(typeof(Func<sbyte, sbyte>));
+            Assert.Equal(20, func(30));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgInt16()
+        {
+            var method = new DynamicMethod("x", typeof(short), new[] { typeof(short) });
+            var il = method.GetILGenerator();
+            il.StoreArg(0, (short)20);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<short, short>)method.CreateDelegate(typeof(Func<short, short>));
+            Assert.Equal(20, func(30));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgUInt16()
+        {
+            var method = new DynamicMethod("x", typeof(ushort), new[] { typeof(ushort) });
+            var il = method.GetILGenerator();
+            il.StoreArg(0, (ushort)20);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<ushort, ushort>)method.CreateDelegate(typeof(Func<ushort, ushort>));
+            Assert.Equal(20, func(30));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgInt32()
+        {
+            var method = new DynamicMethod("x", typeof(int), new[] { typeof(int) });
+            var il = method.GetILGenerator();
+            il.StoreArg(0,20);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<int, int>)method.CreateDelegate(typeof(Func<int, int>));
+            Assert.Equal(20, func(30));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgUInt32()
+        {
+            var method = new DynamicMethod("x", typeof(uint), new[] { typeof(uint) });
+            var il = method.GetILGenerator();
+            il.StoreArg(0, (uint)20);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<uint, uint>)method.CreateDelegate(typeof(Func<uint, uint>));
+            Assert.Equal(20, func(30));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgInt64()
+        {
+            var method = new DynamicMethod("x", typeof(long), new[] { typeof(long) });
+            var il = method.GetILGenerator();
+            il.StoreArg(0, (long)20);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<long, long>)method.CreateDelegate(typeof(Func<long, long>));
+            Assert.Equal(20, func(30));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgUInt64()
+        {
+            var method = new DynamicMethod("x", typeof(ulong), new[] { typeof(ulong) });
+            var il = method.GetILGenerator();
+            il.StoreArg(0, (ulong)20);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<ulong, ulong>)method.CreateDelegate(typeof(Func<ulong, ulong>));
+            Assert.Equal(20, func(30));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgFloat()
+        {
+            var method = new DynamicMethod("x", typeof(float), new[] { typeof(float) });
+            var il = method.GetILGenerator();
+            il.StoreArg(0, (float)20.5);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<float, float>)method.CreateDelegate(typeof(Func<float, float>));
+            Assert.Equal((float)20.5, func((float)30.5));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgDouble()
+        {
+            var method = new DynamicMethod("x", typeof(double), new[] { typeof(double) });
+            var il = method.GetILGenerator();
+            il.StoreArg(0, (double)20.5);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<double, double>)method.CreateDelegate(typeof(Func<double, double>));
+            Assert.Equal((double)20.5, func((double)30.5));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgDecimal()
+        {
+            var method = new DynamicMethod("x", typeof(decimal), new[] { typeof(decimal) });
+            var il = method.GetILGenerator();
+            il.StoreArg(0, (decimal)20.5);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<decimal, decimal>)method.CreateDelegate(typeof(Func<decimal, decimal>));
+            Assert.Equal((decimal)20.5, func((decimal)30.5));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgBoolean()
+        {
+            var method = new DynamicMethod("x", typeof(bool), new[] { typeof(bool) });
+            var il = method.GetILGenerator();
+            il.StoreArg(0, true);
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<bool, bool>)method.CreateDelegate(typeof(Func<bool, bool>));
+            Assert.Equal(true, func(false));
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreArgString()
+        {
+            var method = new DynamicMethod("x", typeof(string), new[] { typeof(string) });
+            var il = method.GetILGenerator();
+            il.StoreArg(0, "Fizz");
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<string, string>)method.CreateDelegate(typeof(Func<string, string>));
+            Assert.Equal("Fizz", func("Buzz"));
+        }
+    }
+}
---
+++ test/build/net45/System.Reflection.Emit.ILGenerator/StoreLocal.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace FluentMethods.UnitTests
+{
+    public partial class ILGeneratorFixture
+    {
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocal()
+        {
+            var method = new DynamicMethod("x", typeof(int), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(int));
+            il.LoadConst(20);
+            il.StoreLocal(local);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<int>)method.CreateDelegate(typeof(Func<int>));
+            Assert.Equal(20, func());
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocalBoolean()
+        {
+            var method = new DynamicMethod("x", typeof(bool), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(bool));
+            il.StoreLocal(local, true);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<bool>)method.CreateDelegate(typeof(Func<bool>));
+            Assert.Equal(true, func());
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocalChar()
+        {
+            var method = new DynamicMethod("x", typeof(char), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(char));
+            il.StoreLocal(local, (char)20);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<char>)method.CreateDelegate(typeof(Func<char>));
+            Assert.Equal((char)20, func());
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocalByte()
+        {
+            var method = new DynamicMethod("x", typeof(byte), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(byte));
+            il.StoreLocal(local, (byte)20);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<byte>)method.CreateDelegate(typeof(Func<byte>));
+            Assert.Equal((byte)20, func());
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocalSByte()
+        {
+            var method = new DynamicMethod("x", typeof(sbyte), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(sbyte));
+            il.StoreLocal(local, (sbyte)20);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<sbyte>)method.CreateDelegate(typeof(Func<sbyte>));
+            Assert.Equal((sbyte)20, func());
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocalUInt16()
+        {
+            var method = new DynamicMethod("x", typeof(ushort), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(ushort));
+            il.StoreLocal(local, (ushort)20);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<ushort>)method.CreateDelegate(typeof(Func<ushort>));
+            Assert.Equal((ushort)20, func());
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocalInt16()
+        {
+            var method = new DynamicMethod("x", typeof(short), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(short));
+            il.StoreLocal(local, (short)20);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<short>)method.CreateDelegate(typeof(Func<short>));
+            Assert.Equal((short)20, func());
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocalInt32()
+        {
+            var method = new DynamicMethod("x", typeof(int), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(int));
+            il.StoreLocal(local,(int)20);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<int>)method.CreateDelegate(typeof(Func<int>));
+            Assert.Equal(20, func());
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocalUInt32()
+        {
+            var method = new DynamicMethod("x", typeof(uint), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(uint));
+            il.StoreLocal(local, (uint)20);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<uint>)method.CreateDelegate(typeof(Func<uint>));
+            Assert.Equal((uint)20, func());
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocalInt64()
+        {
+            var method = new DynamicMethod("x", typeof(long), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(long));
+            il.StoreLocal(local, (long)20);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<long>)method.CreateDelegate(typeof(Func<long>));
+            Assert.Equal((long)20, func());
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocalUInt64()
+        {
+            var method = new DynamicMethod("x", typeof(ulong), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(ulong));
+            il.StoreLocal(local, (ulong)20);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<ulong>)method.CreateDelegate(typeof(Func<ulong>));
+            Assert.Equal((ulong)20, func());
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocalFloat()
+        {
+            var method = new DynamicMethod("x", typeof(float), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(float));
+            il.StoreLocal(local, (float)20.5);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<float>)method.CreateDelegate(typeof(Func<float>));
+            Assert.Equal((float)20.5, func());
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocalDouble()
+        {
+            var method = new DynamicMethod("x", typeof(double), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(double));
+            il.StoreLocal(local, (double)20.5);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<double>)method.CreateDelegate(typeof(Func<double>));
+            Assert.Equal((double)20.5, func());
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocalDecimal()
+        {
+            var method = new DynamicMethod("x", typeof(decimal), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(decimal));
+            il.StoreLocal(local, (decimal)20.5);
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<decimal>)method.CreateDelegate(typeof(Func<decimal>));
+            Assert.Equal((decimal)20.5, func());
+        }
+
+#if NetCore
+        [Xunit.Fact]
+#else
+        [NUnit.Framework.Test]
+#endif
+        public void StoreLocalString()
+        {
+            var method = new DynamicMethod("x", typeof(string), new Type[0]);
+            var il = method.GetILGenerator();
+            var local = il.DeclareLocal(typeof(string));
+            il.StoreLocal(local, "Fizz");
+            il.Emit(OpCodes.Ldloc, local);
+            il.Emit(OpCodes.Ret);
+
+            var func = (Func<string>)method.CreateDelegate(typeof(Func<string>));
+            Assert.Equal("Fizz", func());
+        }
+    }
+}
